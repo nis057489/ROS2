acb0140ccacd194d66e82442acd237ec307ba491
Nicholas Smith <nicholas.j.smith-2@student.uts.edu.au>
2024-12-20 07:01:39 +1000

    init

---
+++ build_and_publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMAGE_NAME="nbembedded/ros2_devenv"
+TAG="latest"
+
+# Build the image
+docker build -t $IMAGE_NAME:$TAG .
+
+# Authenticate (ensure you have already logged in to Docker Hub)
+docker login
+
+# Push the image
+docker push $IMAGE_NAME:$TAG
